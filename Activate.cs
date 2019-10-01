e42046839d91c04e20aa6ef503e430f7bc561d61
Emily Hill <Emily@USERdeMacBook-Pro-4.local>
2019-09-30 19:59:04 -0700

    Activate works

---
+++ Activate.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activate : MonoBehaviour
+{
+
+    public GameObject myObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Active Self: " + myObject.activeSelf);
+        Debug.Log("Active in Hierarchy" + myObject.activeInHierarchy);
+
+        Disable();
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Disable()
+    {
+        StartCoroutine(RemoveAfterSeconds(3, myObject));
+    }
+
+    IEnumerator RemoveAfterSeconds(int seconds, GameObject obj)
+    {
+        yield return new WaitForSeconds(seconds);
+        obj.SetActive(false);
+    }
+
+
+}
---
+++ AxisExample.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AxisExample : MonoBehaviour
+{
+    public GUIText textOutput;
+    public Text horizontalValueDisplayText;
+    public Text verticalValueDisplayText;
+    public float hRange;
+    public float vRange;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        float xPos = h * hRange;
+        float vPos = v * vRange;
+
+        transform.position = new Vector3(xPos, 0, vPos);
+        horizontalValueDisplayText.text = h.ToString("F2");
+        verticalValueDisplayText.text = v.ToString("F2");
+
+    }
+}
