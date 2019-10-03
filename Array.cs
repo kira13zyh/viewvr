b0609ceea347f492dba266c5abaa6990488bb6fe
Emily Hill <Emily@airbears2-10-142-182-40.airbears2.1918.berkeley.edu>
2019-10-03 12:39:02 -0700

    array

---
+++ Array.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Array : MonoBehaviour
+{
+    public GameObject[] players;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Clicked on the cube!");
+        players = GameObject.FindGameObjectsWithTag("Cube");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+
+
+        for (int i = 0; i < players.Length; i++)
+        {
+            Debug.Log("Player Number " + i + " is named " + players[i].name);
+        }
+
+        //rb.useGravity = true;
+    }
+}
