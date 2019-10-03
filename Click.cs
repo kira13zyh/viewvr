efde9f5e395a7452598aaad822b744f2be643704
Emily Hill <Emily@airbears2-10-142-182-40.airbears2.1918.berkeley.edu>
2019-10-03 12:14:08 -0700

    terrain added, rigidbody, onmousedown

---
+++ Click.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Clicked on the cube!");
+        rb.AddForce(-transform.forward * 500f);
+        //rb.useGravity = true;
+    }
+}
