5e24dfb8d9e245dd98530ae3615f0db0d29651a0
Emily Hill <Emily@USERdeMacBook-Pro-4.local>
2019-10-07 08:58:47 -0700

    terrain + script tutorial

---
+++ UsingInstantiate.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UsingInstantiate : MonoBehaviour
+{
+    public GameObject cube;
+    public Transform cube2;
+
+    // Use this for initialization
+    void Start()
+    {
+        Invoke("SpawnObject", 3, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(Input.GetButtonDown("Fire"))
+        //{
+        //    Rigidbody sphere;
+        //    sphere = Instantiate(cube, cube2.position, cube2.rotation) as Rigidbody;
+        //}
+
+    }
+
+    void SpawnObject()
+    {
+        float x = Random.Range(-2.0f, 2.0f);
+        float z = Random.Range(-2.0f, 2.0f);
+        Instantiate(cube, new Vector3(x, 1, z), Quaternion.identity);
+    }
+}
